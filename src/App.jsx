52a10d143bee3c97e42923446c3c310ba28d565e
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-22 17:46:02 +0300

    feat:add nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,18 @@ function App() {
   return (
     <BrowserRouter> 
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route index element={<Homepage />} />
       <Route path="product" element={<Product/>} />
       <Route path="pricing" element={<Pricing />} />
       <Route path="Login" element={<Login />} />
-      <Route path="app" element={<AppLayout/>} />
+      <Route path="app" element={<AppLayout/>} >
+        <Route index element={<p>List</p>}></Route>
+          <Route path="cities" element={<p>List of cities</p>}/>
+          <Route path="countries" element={<p>List of countries</p>}/>
+          <Route path="from" element={<p>Form</p>}/>
+      </Route>
       <Route path="*" element={<PageNotFound/>} />
-    </Routes>
+    </Routes> 
     </BrowserRouter> 
   );
 }
