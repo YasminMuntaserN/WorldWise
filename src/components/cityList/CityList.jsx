fc7a5fc9a3b365c55a2182f8ab8027925d9256fc
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-23 06:37:58 +0300

    refactor: adit the city list class name

--- src/components/cityList/CityList.jsx
+++ src/components/cityList/CityList.jsx
@@ -1,11 +1,24 @@
 import styles from "./CityList.module.css";
 import Spinner from "../Spinner/Spinner"
 import CityItem from "../CityItem/CityItem";
+import Message from "../Message/Message";
+import PropTypes from 'prop-types';
+
+
+// Define prop types for validation
+CityList.propTypes = {
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
 
 function CityList({cities ,isLoading}) {
   if(isLoading) return <Spinner/>
+
+  if(!cities.length) 
+    return <Message message='Add Your first city by clicking on a city on the map'/>;
   return (
-    <ul className={styles.CityList}>
+    <ul className={styles.cityList}>
       {cities.map((city) =>(
         <CityItem city={city} key={city.id}/>
       ))}
