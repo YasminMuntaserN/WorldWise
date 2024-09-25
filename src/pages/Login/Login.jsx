d899f844ffd85d84b8d068d0558c917d5c467f9a
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-25 14:55:08 +0300

    refactor: edit Login page by adding a method to submit add method

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,36 @@
-import styles from "./Login.module.css";
-import { useState } from "react";
+import styles from "./Login.module.css"; 
+import { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Buttons/Button";
+import PageNav from "../../components/PageNav/PageNav"
 
 export default function Login() {
-  // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
+  const { login, isAuthenticated } = useAuth();
+  const [email, setEmail] = useState("Yasmin@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) {
+      login(email, password);  // Trigger login
+      console.log(`${email}, ${password}`);
+    }
+  }
+
+  // Redirect if authenticated after login
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/app", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);  // Listen for authentication status
 
   return (
     <main className={styles.login}>
-      <pageNav/>
-      <form className={styles.form}>
+      <PageNav />
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -31,7 +52,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
