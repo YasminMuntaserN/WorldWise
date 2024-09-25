5acf569d59189f211af27a729269f6b70dba06a5
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-25 07:46:54 +0300

    refactor: edit the props type in the City

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -1,11 +1,11 @@
-import { useParams  } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
-import PropTypes from 'prop-types';
 import { useCities } from "../../contexts/citiesContext";
 import { useEffect } from "react";
 import Spinner from "../Spinner/Spinner";
 import BackButton from "../Buttons/BackButton";
 
+// Format date function
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -13,63 +13,55 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 
-    // Define PropTypes for the city object and its properties
-    City.propTypes = {
-      city : PropTypes.object.isRequired,
-    cityName: PropTypes.string.isRequired,
-    emoji: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    position: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
-};
-
-
 function City() {
-    const { id } = useParams();
-    const {getCity , currentCity , isLoading }=useCities();
-    const { cityName, emoji, date, notes } = currentCity;
+  const { id } = useParams();
+  const { getCity, currentCity, isLoading } = useCities();
 
-    useEffect(function(){
-      getCity(id);
-    } , [id]);
+  useEffect(() => {
+    if (id) getCity(id); // Make sure the id is valid before calling getCity
+  }, [id]);
 
-    if(isLoading) return <Spinner />;
+  if (isLoading) return <Spinner />;
 
-  return ( 
-<div className={styles.city}>
-    <div className={styles.row}>
-      <h6>City name</h6>
-      <h3>
-        <span>{emoji}</span> {cityName}
-      </h3>
-    </div>
+  if (!currentCity) return <p>City not found</p>; // Handle undefined or null currentCity
 
-    <div className={styles.row}>
-      <h6>You went to {cityName} on</h6>
-      <p>{formatDate(date || null)}</p>
-    </div>
+  const { cityName, emoji, date, notes } = currentCity;
 
-    {notes && (
+  return (
+    <div className={styles.city}>
       <div className={styles.row}>
-        <h6>Your notes</h6>
-        <p>{notes}</p>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
       </div>
-    )}
 
-    <div className={styles.row}>
-      <h6>Learn more</h6>
-      <a
-        href={`https://en.wikipedia.org/wiki/${cityName}`}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Check out {cityName} on Wikipedia &rarr;
-      </a>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+      <div>
+        <BackButton />
+      </div>
     </div>
-    <div>
-      <BackButton/>
-    </div>
-  </div>
   );
 }
 
